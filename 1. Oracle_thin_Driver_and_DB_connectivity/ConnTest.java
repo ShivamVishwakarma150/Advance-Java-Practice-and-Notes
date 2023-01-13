f2d2441f4d2871b08fb62791d66291ca6652ffe2
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-01-13 14:17:28 +0530

    13-01-2023

--- 1. Oracle_thin_Driver_and_DB_connectivity/ConnTest.java
+++ 1. Oracle_thin_Driver_and_DB_connectivity/ConnTest.java
@@ -3,11 +3,15 @@ import java.sql.*;
 public class ConnTest {
     public static void main(String[] args) throws Exception {
 
-        
+        // 1. Register JDBC driver s/w
+
+        // a. create JDBC Driver class object
         oracle.jdbc.driver.OracleDriver obj = new oracle.jdbc.driver.OracleDriver();
 
+        // register JDBC driver class object with DriverManager service
         DriverManager.registerDriver(obj);
 
+        //2. Establish the connection
         Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","system","Shivam@12345");
 
         if(con == null) System.out.println("Connection is not established.");
