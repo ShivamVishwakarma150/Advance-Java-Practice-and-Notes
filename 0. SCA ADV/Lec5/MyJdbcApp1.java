5e153fa635045bbf4b34342138fa792ca2c5e6c8
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-01-17 09:32:10 +0530

    17-01-2023

---
+++ 0. SCA ADV/Lec5/MyJdbcApp1.java
@@ -0,0 +1,44 @@
+import java.sql.*;
+
+class MyJdbcApp1{
+    public static void main(String[] args) {
+        Connection con = null;
+        try{
+            Class.forName("oracle.jdbc.OracleDriver");
+
+            System.out.println("Driver Successfully loaded");
+            
+            con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","advjavabatch","myscholars");
+
+            System.out.println("Connection successfully opened!");
+
+            Statement st=con.createStatement();
+
+            ResultSet rs= st.executeQuery("select bookname,bookprice from allbooks");
+
+            while(rs.next()){
+                String bname = rs.getString(1);
+                Double amt = rs.getDouble(2);
+                System.out.println(bname+"\t"+amt);
+            }
+
+        }catch(ClassNotFoundException cnf){
+            System.out.println("Sorry Cannot Load the driver");
+            System.out.println(cnf.getMessage());
+        }catch(SQLException sq){
+            System.out.println("Sorry Problem with DB");
+            System.out.println(sq.getMessage());
+        }
+        finally{
+            if(con!=null){
+                try {
+                    con.close();
+                    System.out.println("Connection Successfully Closed");
+                } catch (SQLException e) {
+                    System.out.println("Sorry Problem in closing the connection");
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
+    }
+}
