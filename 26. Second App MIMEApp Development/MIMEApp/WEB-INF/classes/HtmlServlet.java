64a88f23b5ce15545acb7c2f4a94e12624358c38
Shivam Vishwakarma <arvind.972176@gmail.com>
2023-03-22 11:51:27 +0530

    22-03-2023

---
+++ 26. Second App MIMEApp Development/MIMEApp/WEB-INF/classes/HtmlServlet.java
@@ -0,0 +1,31 @@
+// HtmlServlet.java
+
+package com.shivam.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class HtmlServlet extends HttpServlet
+{
+    protected void service(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+        // set response content Type 
+        res.setContentType("text/html");
+        // get printWriter Stream
+        PrintWriter pw = res.getWriter();
+        // write output content to browser
+        pw.println("<table border='1' align='center'>");
+        pw.println("<tr><th>Player Name </th> <th> Medal</th> <th> category </th> </tr>");
+        pw.println("<tr> <td> Neeraj Chopra </td> <td> Gold </td> <td>Javaline throw </td> </tr>");
+        pw.println("<tr> <td> Meera Bai Chanu </td> <td> Silver </td><td> Weight Lifting </td> </tr>");
+        pw.println("<tr> <td> Ravi kumar Dahiya </td> <td> Silver </td><td> Wrestling </td> </tr>");
+        pw.println("<tr> <td> P.v.Sindhu </td> <td> bronze </td><td> badminton </td> </tr>");
+        pw.println("<tr> <td> Lovlina </td> <td> bronze </td> <td>boxing </td> </tr>");
+        pw.println("<tr> <td> Bajarang </td> <td> bronze </td><td> Wrestling </td> </tr>");
+        pw.println("</table>");
+        //close stream
+        pw.close();
+
+
+    }// service
+}// class
